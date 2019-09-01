ef7987007e3dae605f3e61423fdf918ee35261f9
CyberMancerGamer <54441789+CyberMancerGamer@users.noreply.github.com>
2019-09-01 19:18:39 +0200

    Deleted "build" of proj
    also added a qute picture:)

--- Project 1/proj1_b_w35/proj1_b_w35/main.cpp
+++ Project 1/proj1_b_w35/proj1_b_w35/main.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
+
+std::vector<int> even = {2, 4, 3, 6, 1, 9};
 
 int main() {
 
@@ -13,11 +16,14 @@ int main() {
     int *g  = new int[n];
     int *g_ = new int[n];
 
+    b[0] = 1;
+    b[n] = 1;
 
-
-    for (int i = 1; i<n-1;i++) {
+    for(int i = 1; i<n-1; i++) {
         b_[i] = b[i] - (a[i-1]*c[i-1])/b_[i-1];
         g_[i] = g[i] - (g[i-1]*a[i-1])/b_[i-1];
+    }
+    for(int i = n-1; i>1; i--) {
         v[i]  = (g_[i] - c[i]*v[i+1])/b_[i];
     }
 }
