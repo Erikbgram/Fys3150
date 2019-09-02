840df135b8c23ccffd6fe544ad8d7581515ea4ae
CyberMancerGamer <54441789+CyberMancerGamer@users.noreply.github.com>
2019-09-02 11:13:59 +0200

    Made main.cpp

---
+++ Project 1/Erlends artige saker/main.cpp
@@ -0,0 +1,31 @@
+#include <iostream> // cout, cin, etc.
+#include <ctime> // clock-stuff
+
+using namespace std;
+
+
+int main() {
+  int n = 10;
+  double* a = new double[n];
+
+  a[0] = 1;
+  a[n-1] = 43;
+
+  clock_t start, stop;
+  start = clock();
+
+  for(int i = 1; i < n-1; i++) {
+    a[i] = 85 + i;
+    cout << i << "  " << a[i] << endl;
+  }
+
+  stop = clock();
+  double timeused = (double) (stop-start)/(CLOCKS_PER_SEC);
+  cout << "Time used for loop = " << timeused << "s" << endl;
+
+  cout << "index  position  value\n";
+  for(int i = 0; i < n; i++) {
+    cout << "  " << i << "      " << i+1 << "       " << a[i] << endl;
+  }
+
+}
