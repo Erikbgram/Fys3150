2ec5be2940fd51db73100c0de365871f33500fd4
Erikbgram <54267200+Erikbgram@users.noreply.github.com>
2019-09-02 11:11:40 +0200

    Create Task1B.cpp

---
+++ Project 1/Task1B.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <armadillo>
+
+
+int main{
+  //std::cout << "State sice om matrix: ";
+  //std::cin >> n;
+  //std::cout << arma::mat(3,3);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  return 0;
+
+
+
+
+
+
+
+}
