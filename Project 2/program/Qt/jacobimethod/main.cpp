e8f2c8ff1338c36158d58506f28d7d4c1e90bd17
CyberMancerGamer <54441789+CyberMancerGamer@users.noreply.github.com>
2019-09-27 16:57:48 +0200

    depresjonen inntreffer

--- Project 2/program/Qt/jacobimethod/main.cpp
+++ Project 2/program/Qt/jacobimethod/main.cpp
@@ -6,7 +6,7 @@ using namespace std;
 namespace ch = std::chrono;
 
 //  the offdiag function, using Armadillo
-void offdiag(arma::mat A, int *p, int *q, int n); {
+void offdiag(arma::mat A, int *p, int *q, int n) {
    double max;
    for (int i = 0; i < n; ++i)
    {
@@ -15,15 +15,108 @@ void offdiag(arma::mat A, int *p, int *q, int n); {
            double aij = fabs(A(i,j));
            if ( aij > max)
            {
-              max = aij;  p = i; q = j;
+              max = aij;  p = &i; q = &j;
            }
        }
    }
 }
 
 
+void Jacobi_rotate (arma::mat A, arma::mat R, int k, int l, int n ) {
+  double s, c;
+  if ( A(k,l) != 0.0 ) {
+    double t, tau;
+    tau = (A(l,l) - A(k,k))/(2*A(k,l));
+
+    if ( tau >= 0 ) {
+      t = 1.0/(tau + sqrt(1.0 + tau*tau));
+    } else {
+      t = -1.0/(-tau +sqrt(1.0 + tau*tau));
+    }
+
+    c = 1/sqrt(1+t*t);
+    s = c*t;
+  } else {
+    c = 1.0;
+    s = 0.0;
+  }
+  double a_kk, a_ll, a_ik, a_il, r_ik, r_il;
+  a_kk = A(k,k);
+  a_ll = A(l,l);
+  A(k,k) = c*c*a_kk - 2.0*c*s*A(k,l) + s*s*a_ll;
+  A(l,l) = s*s*a_kk + 2.0*c*s*A(k,l) + c*c*a_ll;
+  A(k,l) = 0.0;  // hard-coding non-diagonal elements by hand
+  A(l,k) = 0.0;  // same here
+  for ( int i = 0; i < n; i++ ) {
+    if ( i != k && i != l ) {
+      a_ik = A(i,k);
+      a_il = A(i,l);
+      A(i,k) = c*a_ik - s*a_il;
+      A(k,i) = A(i,k);
+      A(i,l) = c*a_il + s*a_ik;
+      A(l,i) = A(i,l);
+    }
+//  And finally the new eigenvectors
+    r_ik = R(i,k);
+    r_il = R(i,l);
+
+    R(i,k) = c*r_ik - s*r_il;
+    R(i,l) = c*r_il + s*r_ik;
+  }
+  return;
+} // end of function jacobi_rotate
+
 int main()
 {
+    int n;
     cout << "Hello World!" << endl;
+    cout << "Choose your n: ";
+    cin >> n;
+    cout << endl;
+
+    arma::mat A = arma::mat(n+1, n+1, arma::fill::zeros);
+    arma::mat R = arma::mat(n+1, n+1, arma::fill::eye);
+
+    for(int i = 0; i < n+1; i++) {
+          for(int j = 0; j < n+1; j++) {
+            if(i == j) {
+              A(i,j) = 2;
+            }
+            else if(i == j+1) {
+                A(i,j) = -1;
+            }
+            else if(i == j-1) {
+                A(i,j) = -1;
+            }
+        }
+    }
+    A.print("A: ");
+
+
+
+    //  The final matrix R has the eigenvectors in its row elements, it is set to one
+    //  for the diagonal elements in the beginning, zero else.
+
+
+
+
+    double tolerance = 1.0E-10;
+    int maxiteration = 10000;
+    double maxnondiag = 1;
+
+    int iterations = 0;
+    while ( maxnondiag > tolerance && iterations <= maxiteration)
+    {
+       cout << "Iteration: " << iterations << endl;
+       int p, q;
+       offdiag(A, &p, &q, n);
+       cout << "offdiag complete\n";
+       maxnondiag = A(p,q);
+       Jacobi_rotate(A, R, p, q, n);
+       cout << "rotation complete\n";
+       iterations++;
+    }
+
+
     return 0;
 }
