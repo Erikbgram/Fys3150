fa39fee3088ebbacfcd853da6e488ec9fd653c22
alexanjk <alexandra.kolstad@live.no>
2019-09-27 23:37:17 +0200

    så mange endringer at jeg ikke vet hva jeg skal skrive, vær så god

--- Project 2/program/plot_data.py
+++ Project 2/program/plot_data.py
@@ -1,36 +1,53 @@
 """
-Last changed: 27.09.2019 18:00 by Erlend
+Last changed: 27.09.2019 23:19 by Alexandra
 """
 
 #imports
 import sys
-import pylab as pl
+import pylab as plt
 
 #create lists for file-data
-x = []
-y = []
+n = []
+iterations = []
+time_span_eig_sym = []
+time_span_ours = []
 
-filename = sys.argv[1] #multiple files useful, we ask for user-input
+filename = sys.argv[1][:-4] #multiple files useful, we ask for user-input
 
-with open(filename) as infile:
+with open(filename + ".txt") as infile:
     infile.readline() #discard 1st line
     lines = infile.readlines() #read whole file
     for line in lines:
         words = line.split(", ") #split line per each ", "
-        x.append(float(words[0])) #convert and append 1st element
-        y.append(float(words[1])) #convert and append 2nd element
+        n.append(float(words[0])) #convert and append 1st element
+        iterations.append(float(words[1])) #convert and append 2nd element
+        time_span_eig_sym.append(float(words[2]))
+        time_span_ours.append(float(words[3]))
 
 #convert lists to arrays for ease-of-use
-x = pl.array(x)
-y = pl.array(y)
+n = plt.array(n)
+iterations = plt.array(iterations)
+time_span_eig_sym = plt.array(time_span_eig_sym)
+time_span_ours = plt.array(time_span_ours)
+
+plt.figure(figsize=(12,9)) #change figuresize for better quality image
+plt.plot(n, iterations) #plot of iterations as function of dimension
+plt.grid()#add grid to plot
+plt.title("Plot of iterations from " + filename) #add title
+plt.xlabel("n") #add x-label
+plt.ylabel("Iterations") #add y-label
+plt.legend() #enable labels on plot
+plt.savefig("../iterations-" + filename + ".png") #saves plot as image
+plt.show() #show plot
 
 
-pl.figure(figsize=(12,9)) #change figuresize for better quality image
-pl.plot(x,y) #plot of y(x)
-pl.grid()#add grid to plot
-pl.title("Plot of data from" + filename) #add title
-pl.xlabel("x") #add x-label
-pl.ylabel("y(x)") #add y-label
-pl.legend() #enable labels on plot
-pl.savefig("data_from_" + filename + ".png") #saves plot as image
-pl.show() #show plot
+plt.figure(figsize=(12,9)) #change figuresize for better quality image
+plt.plot(n,time_span_eig_sym, label = "eig_sym") #plot of timespan for eig_sym from Armadillo as function of dimension
+plt.plot(n,time_span_ours, label = "Jacobi method") #plot of timespan for Jacobi method as function of dimension
+plt.grid()#add grid to plot
+plt.title("Plot of timespan from " + filename) #add title
+plt.xlabel("n") #add x-label
+plt.ylabel("Time [s]") #add y-label
+plt.legend() #enable labels on plot
+plt.savefig("../timespan-" + filename + ".png") #saves plot as image
+plt.show() #show plot
