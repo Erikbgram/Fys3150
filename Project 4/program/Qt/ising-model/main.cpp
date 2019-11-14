0767cb72857cc63f5af7018373cf1c48f98b47d4
CyberMancerGamer <54441789+CyberMancerGamer@users.noreply.github.com>
2019-11-14 17:28:21 +0100

    did stuff

--- Project 4/program/Qt/ising-model/main.cpp
+++ Project 4/program/Qt/ising-model/main.cpp
@@ -7,13 +7,14 @@
 #include <random>
 #include <armadillo>
 #include <chrono>
+#include <fstream>
 //#include <mpi.h>
 #define kB 1
 using namespace std;
 namespace ch = std::chrono;
 
 // Functions
-void initLattice(arma::Mat<int> &M,int L) { // Initializes the lattice
+void initLattice(arma::Mat<int> &M, int L) { // Initializes the lattice
   random_device rd;
   mt19937_64 generator(rd());
   uniform_int_distribution<int> distribution(0,1);
@@ -21,38 +22,68 @@ void initLattice(arma::Mat<int> &M,int L) { // Initializes the lattice
   for(int i = 0; i < L; i++) {
     for(int j = 0; j < L; j++) {
       M(i,j) = distribution(generator);
-      cout << M(i,j) << " "; // For checking the distribution
+      if(M(i,j) == 0) { // We want spins -1 and 1, not 0 and 1
+        M(i,j) = -1;
+      }
     }
-    cout << endl; // For checking the distribution
+  }
+  M.print();
+}
+
+void flip(int &spin) { // Flips spin-value
+  if(spin==1) {
+    spin = -1;
+  }
+  else {
+    spin = 1;
   }
 }
 
-void flip(int &spin) { // Generates a random spin-value
-  random_device rd;
-  mt19937_64 generator(rd());
-  uniform_int_distribution<int> distribution(0,1);
-  spin = distribution(generator);
+int E(arma::Mat<int> M, int L, int k, int l) { // Calculates energy
+  int sum = 0;
+  for(int i = -1; i < 2; i++) {
+    // k
+    if(k==0) {
+      sum += M(k+abs(i),l)*M(k,l); // Periodic boundary condition.  abs(i) makes k-1 into k+1
+    }
+    else if(k==L-1) {
+      sum += M(k-abs(i),l)*M(k,l); // Periodic boundary condition. -abs(i) makes k+1 into k-1
+    }
+    else {
+      sum += M(k+i,l)*M(k,l);
+    }
+    // l
+    if(l==0) {
+      sum += M(k,l+abs(l))*M(k,l); // Periodic boundary condition.  abs(i) makes l-1 into l+1
+    }
+    else if(l==L-1) {
+      sum += M(k,l-abs(l))*M(k,l); // Periodic boundary condition. -abs(i) makes l+1 into l-1
+    }
+    else {
+      sum += M(k,l+i)*M(k,l);
+    }
+  }
+  return sum;
 }
 
-double delE(int k,int l) {
-  // Do Stuff
-  return 0;
-}
 
 void MonteCarlo(int n, double a, double b, double  &integral, double  &std) {
   // Do stuff
 }
 
-int Metropolis(double delE,double T=1.0) {
+bool Metropolis(double delE,double T=1.0) {
   random_device rd;
   mt19937_64 generator(rd());
   uniform_real_distribution<double> distribution(0.0,1.0);
 
   double r = distribution(generator);
+  cout << r << ", " << exp(delE/(kB*T)) << endl;
   if(r <= exp(delE/(kB*T))) {
-  return 1;
+    return 1;
   }
-  return 0;
+  else  {
+    return 0;
+  }
 }
 
 int main(int argc, char *argv[]) { // Main function
@@ -63,9 +94,6 @@ int main(int argc, char *argv[]) { // Main function
   arma::Mat<int> lattice(L,L,arma::fill::zeros);
   initLattice(lattice, L);
 
-  // Choose and flip spin
-
-
 
 
   //----------------------------------------------------------------------------
@@ -74,14 +102,25 @@ int main(int argc, char *argv[]) { // Main function
   int Ej = 0;
   random_device rd;
   mt19937_64 generator(rd());
-  uniform_int_distribution<int> distribution(0,n-1);
+  uniform_int_distribution<int> distribution(0,L-1);
+
+  fstream outfile;
+  outfile.open("../../energy.txt", fstream::out);
+  outfile << "Energy" << endl;
+
   for(int i = 0; i < n; i++) {
+    // Choose and flip spin
     int k = distribution(generator);
     int l = distribution(generator);
-    flip(lattice(k,l));
-    Ej = 0; // CALCULATE
-    Metropolis(delE(k,l));
+    Ej = E(lattice, L, k, l); // Energy pre-flip
+    flip(lattice(k,l)); // Flipping
+    Ei = E(lattice, L, k, l); // Energy post-flip
+    //Metropolis(delE(k,l));
+    bool temp = Metropolis(Ei-Ej);
+    cout << "Metropolis: " << temp << endl;
     // Update averages
+    outfile << Ei << endl;
+    //lattice.print();
   }
   cout << "Test" << endl;
 
@@ -91,5 +130,7 @@ int main(int argc, char *argv[]) { // Main function
   /// mean magnetization |M|
   /// specific heat Cv
   /// susceptibility chi (X)
+
+  outfile.close();
 
 }
